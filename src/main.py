41d209ee4df6f378e136f46a04fe9e7119e3841d
Rohan Thorat <rohan@mac.myfiosgateway.com>
2024-11-01 17:23:00 -0400

    update server start

--- src/main.py
+++ src/main.py
@@ -62,6 +62,6 @@ if __name__ == "__main__":
     inverted_index = indexer.get_inverted_index()
 
 
-    uvicorn.run(app, host="127.0.0.1", port=9999)
+    uvicorn.run(app, host="0.0.0.0", port=9999)
 
 
